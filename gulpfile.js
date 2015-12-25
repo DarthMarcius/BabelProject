c5ddc5eae71bb1ef6dc49e64756cebfb8238083e
Markiyan Pyts (mpyts) <markiyan.pyts@osf-global.com>
2015-12-25 11:16:59 +0200

    initialized the project

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('build', function () {
+    return browserify({entries: './script/app.js', debug: true})
+        .transform('babelify', {presets: ['es2015']})
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./compiled/js'));
+});
+
+gulp.task('css', function () {
+    var postcss    = require('gulp-postcss');
+    var sourcemaps = require('gulp-sourcemaps');
+    var rucksack = require('rucksack-css');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./style/**/*.css')
+        .pipe( sourcemaps.init() )
+        .pipe( postcss([ rucksack(),  autoprefixer({
+        	browsers: ['last 10 versions']
+        })]) )
+        .pipe( sourcemaps.write('.') )
+        .pipe( gulp.dest('./compiled/css') );
+});
+
+gulp.task('watch', function () {
+   gulp.watch('./style/**/*.css', ['css']);
+   gulp.watch('./script/**/*.js', ['css']);
+});
---
+++ script/app.js
@@ -0,0 +1,3 @@
+import multiply from './multiply';
+console.log(multiply(2, 3)); // => 2 * 3 = 6
+var [a, b, c] = [1 , 2, 3];
